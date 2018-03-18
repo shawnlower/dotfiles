edaa602c440820d626184882f0e51a9fc27889cf
Shawn Lower <shawn@shawnlower.com>
2018-03-18 11:40:16 -0400

    quick project jump/init script for bash, w/ completion

---
+++ bash/.bash_funcs/pj.bash
@@ -0,0 +1,62 @@
+################################################################################
+#!/bin/bash
+#
+# Bash function to start working on a project
+#
+# Assumes projects are rooted under $PROJ_DIR
+# Running 'pj proj_foo' will do the following:
+# - Export 'PJ_NAME=proj_foo'
+# - cd $PJ_DIR/proj_foo
+# - source any $PJ_DIR/.pj/common.sh
+# - source any $PJ_DIR/$PJ_NAME/.pj_init.sh scripts
+#
+################################################################################
+# Example .pj_init.sh:
+#     cd rest-app.git
+#     
+#     activate=./venv/bin/activate
+#     if [[ -z "$VIRTUAL_ENV" ]]; then
+#       source venv/bin/activate
+#     fi
+#
+#     git status
+#
+################################################################################
+
+
+export PJ_DIR=~/projects/
+
+# This script is used to modify our current environment, so
+# fail if we're called as a subshell.
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    echo "ERROR: This script must be sourced." >&2
+    usage
+    exit 1
+fi
+
+function pj(){
+    if [[ $# -ne 1 ]]; then
+        usage
+        exit 1
+    fi
+
+    export PJ_NAME=$1
+
+    dir=${PJ_DIR}/${PJ_NAME}
+    if [[ ! -d "$dir" ]]; then
+        echo "Missing project dir: $dir"
+        exit 1
+    fi
+
+    cd $dir
+    for pj_init in "$PJ_DIR/.pj/common.sh" "$dir/.pj_init.sh"; do
+        if [[ -f $pj_init ]]; then
+            echo "Found project script: $pj_init" >&2
+            source $pj_init
+        fi
+    done
+}
+
+function usage(){
+  echo "Usage: source $0; pj my_project" >&2
+}
